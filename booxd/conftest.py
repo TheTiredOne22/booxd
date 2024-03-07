44f11820903a7365bede37bd42cc68f0aa1c53ce
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2024-03-07 13:27:19 +0100

    first commit

---
+++ booxd/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from booxd.users.models import User
+from booxd.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
